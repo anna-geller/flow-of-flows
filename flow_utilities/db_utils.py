4c79be9003ee1b3ac5e55a47c03366e3e84ff1fe
Anna Geller <anna.m.geller@gmail.com>
2021-11-19 16:07:29 +0100

    Revert "Update db_utils.py"
    This reverts commit 73810ba2db73240780e5ac6f17127be7bd3b6cec.

--- flow_utilities/db_utils.py
+++ flow_utilities/db_utils.py
@@ -26,5 +26,6 @@ def load_df_to_db(df: pd.DataFrame, table_name: str, schema: str = "jaffle_shop"
     conn_string = get_db_connection_string()
     db_engine = create_engine(conn_string)
     conn = db_engine.connect()
-    df.to_sql(table_name, schema=schema, con=db_engine, if_exists="replace", index=False)
+    conn.execute(f"TRUNCATE TABLE {schema}.{table_name};")
+    df.to_sql(table_name, schema=schema, con=db_engine, if_exists="append", index=False)
     conn.close()
